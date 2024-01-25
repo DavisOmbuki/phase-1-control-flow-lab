360f652da1d08e8cf2d44d316bf868ec25ab7b45
DavisOmbuki <davyomonyi@gmail.com>
2024-01-26 00:35:08 +0300

    the commit

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  let distance = 1000; // Replace this with the actual distance desired by the passenger
+
+  if (distance <= 250) {
+    return "Your destination is very close, I'll be there shortly!";
+  } else if (distance <= 2500) {
+    return "You've got a long way to go, but I'll get you there in no time!";
+  } else {
+    return "Wow, your destination is far away! But don't worry, I'll make sure you get there on time.";
+  }
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  let destination = "New York"; // Replace this with the actual destination desired by the passenger
+
+  return destination === "New York" ? "Let's go to the city!" : "Sorry, I can't go to the city.";
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  let tip = 20; // Replace this with the actual tip amount given by the passenger
+
+  switch (tip) {
+    case 10:
+      return "Thank you for your generous tip!";
+    case 15:
+      return "Your tip is appreciated!";
+    case 20:
+      return "Wow, what a kind passenger!";
+    default:
+      return "Thank you for your tip!";
+  }
 }
